8fac5bf4f7d437be75aa9648d2c41f4cac6b2831
jb1011 <demenet.jb@hotmail.fr>
2019-10-01 16:53:49 +0200

    template almost ok

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,25 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require vendor/jquery/dist/jquery.min
+//= require vendor/popper.js/dist/umd/popper.min
+//= require vendor/bootstrap/dist/js/bootstrap.min
+//= require vendor/rellax/rellax.min
+//= require vendor/@fancyapps/fancybox/dist/jquery.fancybox.min
+//= require vendor/sticky-kit/dist/sticky-kit.min
+//= require vendor/imagesloaded/imagesloaded.pkgd.min
+//= require vendor/isotope-layout/dist/isotope.pkgd.min
+//= require vendor/isotope-packery/packery-mode.pkgd.min
+//= require vendor/aos/dist/aos
+//= require js/theme
