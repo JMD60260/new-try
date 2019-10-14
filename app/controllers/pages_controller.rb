c5e648020ae045f3a04970fffecd9925b3422bd5
jb1011 <demenet.jb@hotmail.fr>
2019-10-14 15:49:54 +0200

    all good

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,18 @@ class PagesController < ApplicationController
 	def index
 	end
 
+	def contact
+	end
+
 	def page2
 	end
 
 	def page3
+	end
+
+	def page4
+	end
+
+	def page5
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
+  get '/contact', :to=> 'pages#contact'
   get '/page2', :to=> 'pages#page2'
   get '/page3', :to=> 'pages#page3'
+  get '/page4', :to=> 'pages#page4'
+  get '/page5', :to=> 'pages#page5'
 end
